ceed8c2448e99f701a688c620bf068f3b355883f
lordfundarbyrd <lordfundarbyrd@gmail.com>
2023-02-20 13:20:30 -0500

    Add bar chart and tooltips

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const FRAME1 = d3.select("#vis1")
         .attr("width", FRAME_WIDTH)
         .attr("class", "frame");
 
-const csvData = d3.csv("data/scatter-data.csv");
+const csvData1 = d3.csv("data/scatter-data.csv");
 
 function plotPoints(data) {
     const MAX_X = d3.max(data, (d) => {
@@ -61,8 +61,8 @@ function plotPoints(data) {
     return [X_SCALE, Y_SCALE];
 }
 
-function build_plot() {
-    csvData.then((data) => {
+function buildScatterPlot() {
+    csvData1.then((data) => {
       console.log(data);
 
       const [X_SCALE, Y_SCALE] = plotPoints(data);
@@ -100,4 +100,100 @@ function build_plot() {
         .addEventListener("click", addPoint);
     });
 }
-build_plot()
+buildScatterPlot()
+
+const FRAME2 = d3.select("#vis2")
+    .append("svg")
+        .attr("height", FRAME_HEIGHT)
+        .attr("width", FRAME_WIDTH)
+        .attr("class", "frame");
+
+const csvData2 = d3.csv("data/bar-data.csv");
+
+function buildBarPlot() {
+    csvData2.then((data) => {
+        console.log(data);
+
+        const X_SCALE = d3
+          .scaleBand()
+          .range([0, VIS_WIDTH])
+          .domain(
+            data.map(function (d) {
+              return d.category;
+            })
+          )
+          .padding(0.2);
+
+        const MAX_Y = d3.max(data, (d) => {
+          return parseInt(d.amount);
+        });
+
+        const Y_SCALE = d3
+          .scaleLinear()
+          .domain([0, MAX_Y + 1])
+          .range([VIS_HEIGHT, 0]);
+
+        FRAME2.selectAll("bars")
+          .data(data)
+          .enter()
+          .append("rect")
+          .attr("x", function (d) {
+            return X_SCALE(d.category) + MARGINS.left;
+          })
+          .attr("y", function (d) {
+            return Y_SCALE(d.amount) + MARGINS.bottom;
+          })
+          .attr("width", X_SCALE.bandwidth())
+          .attr("height", function (d) {
+            return VIS_HEIGHT - Y_SCALE(d.amount);
+          })
+          .attr("class", "bar");
+
+        FRAME2.append("g")
+          .attr(
+            "transform",
+            "translate(" + MARGINS.left + "," + (VIS_HEIGHT + MARGINS.top) + ")"
+          )
+          .call(d3.axisBottom(X_SCALE).ticks(5))
+          .attr("font-size", 12);
+
+        FRAME2.append("g")
+          .attr("transform", `translate( ${MARGINS.bottom}, ${MARGINS.left})`)
+          .call(d3.axisLeft(Y_SCALE).ticks(5))
+          .attr("font-size", 12);
+
+        const TOOLTIP = d3
+          .select("#vis2")
+          .append("div")
+          .attr("class", "tooltip")
+          .style("opacity", 0);
+
+        // Define event handler functions for tooltips
+        function handleMouseover(event, d) {
+          // on mouseover, make opaque
+          TOOLTIP.style("opacity", 1);
+        }
+
+        function handleMousemove(event, d) {
+          // position the tooltip and fill in information
+          TOOLTIP.html("Category: " + d.category + "<br>Amount: " + d.amount)
+            .style("left", event.pageX + 10 + "px") //add offset
+            // from mouse
+            .style("top", event.pageY - 50 + "px");
+        }
+
+        function handleMouseleave(event, d) {
+          // on mouseleave, make transparant again
+          TOOLTIP.style("opacity", 0);
+        }
+
+        // Add event listeners
+        FRAME2.selectAll(".bar")
+          .on("mouseover", handleMouseover) //add event listeners
+          .on("mousemove", handleMousemove)
+          .on("mouseleave", handleMouseleave);
+
+    })
+}
+
+buildBarPlot();
